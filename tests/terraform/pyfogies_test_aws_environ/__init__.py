be1040d5d68217987b8f1944abb34a4779262dfa
James Fogarty <jayfo@users.noreply.github.com>
2026-08-14 18:18:39 -0700

    Flatten for Simplicity

--- tests/terraform/pyfogies_test_aws_environ/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Package for the pyfogies-test-aws-environ shared fixture."""
-
-from tests.terraform.pyfogies_test_aws_environ.pyfogies_test_aws_environ import (
-    pyfogies_test_aws_environ,
-)
-
-__all__ = ["pyfogies_test_aws_environ"]
