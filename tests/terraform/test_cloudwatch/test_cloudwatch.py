13ce6976230281f85a1948d89a31b3500553ca85
James Fogarty <jayfo@users.noreply.github.com>
2026-08-15 07:59:04 -0700

    Use Backend Status

--- tests/terraform/test_cloudwatch/test_cloudwatch.py
+++ tests/terraform/test_cloudwatch/test_cloudwatch.py
@@ -24,7 +24,7 @@ from fogies.tools.terraform import (
 )
 from fogies.retry import readiness_poll_short
 from fogies.typing import boto_client_logs
-from tasks.paths import PATH_STAGING_BINARY_CACHE
+from tasks.paths import PATH_STAGING_BINARY_CACHE, PATH_TEST_BACKEND_STATUS
 from tests.pyfogies_tests_config import PyfogiesTestsConfig
 from tests.terraform.backend import PyfogiesTestTerraformBackendStates
 
@@ -56,12 +56,12 @@ def cloudwatch_output(
     tfbackend_path = tmp_path / "test-cloudwatch.tfbackend"
     tfvars_path = tmp_path / "test-cloudwatch.tfvars.json"
 
+    backend = pyfogies_test_backend[PyfogiesTestTerraformBackendStates.TEST_CLOUDWATCH.value]
+
     with (
         terraform_tfbackend(
             path=tfbackend_path,
-            backend=pyfogies_test_backend[
-                PyfogiesTestTerraformBackendStates.TEST_CLOUDWATCH.value
-            ],
+            backend=backend,
         ) as tfbackend_path,
         terraform_tfvars(
             path=tfvars_path,
@@ -77,6 +77,8 @@ def cloudwatch_output(
             module_path=module_path,
             tfvars_path=tfvars_path,
             tfbackend_path=tfbackend_path,
+            backend=backend,
+            backend_status_path=PATH_TEST_BACKEND_STATUS,
             init_on_entry=True,
             init_params=InitParams(upgrade=True, reconfigure=True),
             apply_on_entry=True,
